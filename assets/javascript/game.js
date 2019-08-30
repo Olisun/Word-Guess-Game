2f353912599568fc692f56a5a78c0aa1dbbe844f
Oliver Sun <olisun@mac.com>
2019-08-29 19:31:16 -0700

    created html, css and js files. laid out initial structure of the game page. added variables and initial logic to js file. long way to go...

---
+++ assets/javascript/game.js
@@ -0,0 +1,36 @@
+var letterOne = document.getElementById('letter1');
+var letterTwo = document.getElementById('letter2');
+var letterThree = document.getElementById('letter3');
+var letterFour = document.getElementById('letter4');
+var letterFive = document.getElementById('letter5');
+var letterSix = document.getElementById('letter6');
+var letterSeven = document.getElementById('letter7');
+var letterEight = document.getElementById('letter8');
+var userMessage = document.getElementById('message');
+var remainingGuesses = document.getElementById('guesses-remaining')
+var guessesRemaining = 10;
+
+
+document.onkeyup = function(event) {
+  var keypress = event.key;
+  if (keypress === 'd') {
+    letterOne.innerHTML = 'D'
+  };
+  if (keypress === 'o') {
+    letterTwo.innerHTML = 'O'
+  };
+  if (keypress === 'g') {
+    letterThree.innerHTML = 'G'
+  };
+  if (keypress === 'd' && keypress === 'd')
+}
+
+
+
+// var arrayItems = ['letter1', 'letter2', 'letter3', 'letter4', 'letter5', 'letter6', 'letter7', 'letter8'];
+// const singleItem = [];
+// arrayItems.forEach(function(thingy) {
+//   singleItem.push(document.getElementById(thingy));
+// })
+
+// singleItem.forEach(thingy);
