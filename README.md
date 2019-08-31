ef3019f159af13a15578ee676af7ee069bce03b7
Oliver Sun <olisun@mac.com>
2019-08-30 22:12:32 -0700

    added READMe. refactored some js. pushing what I have to GH

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # Word-Guess-Game
-JavaScript Homework Assignment 1
+
+## Page preview: 
+![](assets/images/screenshot.png)
+
+
+## About the project:
+This is the more challegning of the 2 assignment options we had to pick 1 from. I did the easier one first just to get the homework done. I had extra time so I attempted to build this game. I don't have all the features yet and I basically ran out of time (one more day would have gotten it built). I'm pushing what I have now. I will finish this on my own however because I liked it. Below are the specs from class.
+
+* Choose a theme for your game! In the demo, we picked an 80s theme: 80s questions, 80s sound and an 80s aesthetic. You can choose any subject for your theme, though, so be creative!
+* Use key events to listen for the letters that your players will type.
+* Display the following on the page:
+* Press any key to get started!
+* Wins: (# of times user guessed the word correctly).
+* If the word is madonna, display it like this when the game starts: _ _ _ _ _ _ _.
+* As the user guesses the correct letters, reveal them: m a d o _ _ a.
+* NMumber of Guesses Remaining: (# of guesses remaining for the user).
+* Letters Already Guessed: (Letters the user has guessed, displayed like L Z Y H).
+* After the user wins/loses the game should automatically choose another word and make the user play it.
+
+
+## Techologies used to build:
+  * HTML
+  * CSS
+  * Javascript
+
+## Methodology:
+Started on this last night. I used the js methods learned from this week to try and build out according to the specs. Ran into various issues with the count features (my code was double copunting guesses remaining). Sorry my comments in the js file are sparse. Definitly will finish this. Added this (and the other option for the assignment) to my Basic Portfolio (New), Bootstrap and Responsive sites (but need to go back and fix the stlying....)
+
+
+
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,10 @@
 var letterOne = document.getElementById('letter1');
 var letterTwo = document.getElementById('letter2');
 var letterThree = document.getElementById('letter3');
-var letterFour = document.getElementById('letter4');
-var letterFive = document.getElementById('letter5');
-var letterSix = document.getElementById('letter6');
-var letterSeven = document.getElementById('letter7');
-var letterEight = document.getElementById('letter8');
-var userMessage = document.getElementById('message');
 var remainingGuesses = document.getElementById('guesses-remaining')
-var guessesRemaining = 10;
-var guessesRemaining2 = 10;
-var guessesRemaining3 = 10;
+var guessesRemaining = 5;
+var guessesRemaining2 = 5;
+var guessesRemaining3 = 5;
 var correctGuesses = document.getElementById('correct-guesses');
 var correctGuessesNum = 0;
 var correctGuessesNum2 = 0;
@@ -25,53 +19,46 @@ document.onkeyup = function(event) {
   var keypress = event.key;
   var ol = document.querySelector('ol');
   var li = document.createElement('li');
-  var correctTotal = correctGuessesNum + correctGuessesNum2;
-  var wrongTotal = wrongGuessesNum + wrongGuessesNum2;
+
+
+  li.appendChild(document.createTextNode(keypress));
+  ol.appendChild(li);
+
 
   if (keypress === 'd') {
     letterOne.innerHTML = 'D'
     guessesRemaining--
     correctGuessesNum++
     remainingGuesses.innerText = guessesRemaining
-    userMessage.innerHTML = 'Nice Guess!'
+
   } else {
     guessesRemaining--
-    wrongGuessesNum++
     remainingGuesses.innerText = guessesRemaining
-      // userMessage.innerHTML = 'Sorry but try again!'
+
   }
 
-  if (keypress === 'o') {
-    letterTwo.innerHTML = 'O'
+  if (keypress === 'e') {
+    letterTwo.innerHTML = 'E'
     guessesRemaining2--
     correctGuessesNum2++
     remainingGuesses.innerText = guessesRemaining2
-    userMessage.innerHTML = 'Nice Guess!'
+
   } else {
     guessesRemaining2--
-    wrongGuessesNum2++
     remainingGuesses.innerHTML = guessesRemaining2
-    userMessage.innerHTML = 'Sorry but try again!'
+
   }
 
-  // if (keypress === 'g') {
-  //   letterThree.innerHTML = 'G'
-  //   guessesRemaining3--
-  //   correctGuessesNum3++
-  //   // remainingGuesses.innerText = guessesRemaining2
-  //   // userMessage.innerHTML = 'Nice Guess!'
-  // } else {
-  //   guessesRemaining3--
-  //   wrongGuessesNum3++
-  //   // remainingGuesses.innerHTML = guessesRemaining2
-  //   // userMessage.innerHTML = 'Sorry but try again!'
-  // }
+  if (keypress === 'f') {
+    letterThree.innerHTML = 'F'
+    guessesRemaining3--
+    correctGuessesNum3++
+    remainingGuesses.innerText = guessesRemaining3
 
-  li.appendChild(document.createTextNode(keypress));
-  ol.appendChild(li);
+  } else {
+    guessesRemaining3--
+    remainingGuesses.innerHTML = guessesRemaining3
+  }
 
 
-
-  correctGuesses.innerHTML = `Your correct guesses: ${correctTotal}`
-  wrongGuesses.innerHTML = `Your incorrect guesses: ${wrongTotal}`;
 }
