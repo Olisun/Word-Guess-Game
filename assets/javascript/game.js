a1bbae65b9cd19153c44f035e2051ebf90f64eb5
Oliver Sun <olisun@mac.com>
2019-08-31 13:21:21 -0700

    refactored js code to fix bug counting guesses remaining. added game over function and you win function. deleted unnecessary divs in html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,59 @@
+// Declaring global variables
 var letterOne = document.getElementById('letter1');
 var letterTwo = document.getElementById('letter2');
 var letterThree = document.getElementById('letter3');
 var remainingGuesses = document.getElementById('guesses-remaining')
 var guessesRemaining = 5;
-var guessesRemaining2 = 5;
-var guessesRemaining3 = 5;
-var correctGuesses = document.getElementById('correct-guesses');
-var correctGuessesNum = 0;
-var correctGuessesNum2 = 0;
-var correctGuessesNum3 = 0;
-var wrongGuesses = document.getElementById('wrong-guesses');
-var wrongGuessesNum = 0;
-var wrongGuessesNum2 = 0;
+var correctKeysPressed = 0;
 
-
-
+// Declaring keypress function
 document.onkeyup = function(event) {
+  // Declaring local variables
   var keypress = event.key;
   var ol = document.querySelector('ol');
   var li = document.createElement('li');
 
-
+  // Appending corredct keys press to DOM
   li.appendChild(document.createTextNode(keypress));
   ol.appendChild(li);
 
-
+  // Setting actions for keys pressed
   if (keypress === 'd') {
     letterOne.innerHTML = 'D'
     guessesRemaining--
-    correctGuessesNum++
+    correctKeysPressed++
     remainingGuesses.innerText = guessesRemaining
-
   } else {
     guessesRemaining--
     remainingGuesses.innerText = guessesRemaining
-
   }
 
   if (keypress === 'e') {
     letterTwo.innerHTML = 'E'
-    guessesRemaining2--
-    correctGuessesNum2++
-    remainingGuesses.innerText = guessesRemaining2
-
-  } else {
-    guessesRemaining2--
-    remainingGuesses.innerHTML = guessesRemaining2
-
+    correctKeysPressed++
   }
 
   if (keypress === 'f') {
     letterThree.innerHTML = 'F'
-    guessesRemaining3--
-    correctGuessesNum3++
-    remainingGuesses.innerText = guessesRemaining3
-
-  } else {
-    guessesRemaining3--
-    remainingGuesses.innerHTML = guessesRemaining3
+    correctKeysPressed++
   }
 
+  // Setting function for game over when # of guesses is hits 5
+  function gameOver() {
+    if (guessesRemaining < 1) {
+      alert('Game Over')
+    }
+  }
+
+  // Setting function for alerting user win all 3 letters are typed.
+  function youWin() {
+    if (correctKeysPressed === 3) {
+      alert('You guessed it!')
+    }
+  }
+
+  // calling the 2 functions aobe to activate them.
+  gameOver();
+  youWin();
 
 }
